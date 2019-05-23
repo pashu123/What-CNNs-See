bcc843a71c134bfc9b0a1985685593a6ed49ef1f
pashu123 <pk5561@gmail.com>
2019-05-23 22:33:55 +0530

    new data

--- train_mnist.py
+++ train_mnist.py
@@ -2,42 +2,51 @@
 
 import numpy as np
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout , Flatten, Activation,Input
 from keras.layers import Conv2D,MaxPooling2D
 from keras import backend as K
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from keras.utils import np_utils
+from sklearn.preprocessing import MinMaxScaler
 
+
+## about data
 batch_size = 128
-num_classes = 10
-epochs = 10
+num_classes = 26
+epochs = 25
+
+df = pd.read_csv("handwritten.csv").astype('float32')
+df.rename(columns={'0':'label'}, inplace=True)
+
+print('Loading done')
+# Splite data the X - Our data , and y - the prdict label
+X = df.drop('label',axis = 1)
+y = df['label']
 
  #input image dimensions
 img_rows, img_cols = 28, 28
 
 # the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+standard_scaler = MinMaxScaler()
+standard_scaler.fit(X_train)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+X_train = standard_scaler.transform(X_train)
+X_test = standard_scaler.transform(X_test)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+input_shape = (28,28,1)
+
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -55,10 +64,10 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,
+model.fit(X_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test))
+          validation_data=(X_test, y_test))
 
-model.save('mnistcnn.h5')
+model.save('handwritten.h5')
