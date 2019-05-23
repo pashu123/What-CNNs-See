49622dbb9887ef449f7aff61a2db436c987c0ce7
pashu123 <pk5561@gmail.com>
2019-05-23 20:49:38 +0530

    new model

---
+++ activation_maximization.py
@@ -0,0 +1,32 @@
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential, Model,load_model
+from keras.layers import Dense, Dropout, Flatten, Activation, Input
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+## Load the model the see the activation map
+model = load_model('mnistcnn.h5')
+
+
+# visualize the dense layer
+from vis.visualization import visualize_activation
+from vis.utils import utils
+from keras import activations
+from matplotlib import pyplot as plt
+
+plt.rcParams['figure.figsize'] = (18, 6)
+
+# Utility to search for layer index by name. 
+# Alternatively we can specify this as -1 since it corresponds to the last layer.
+layer_idx = utils.find_layer_idx(model, 'preds')
+
+# Swap softmax with linear
+model.layers[layer_idx].activation = activations.linear
+model = utils.apply_modifications(model)
+
+# This is the output node we want to maximize.
+filter_idx = 0
+img = visualize_activation(model, layer_idx, filter_indices=filter_idx)
+plt.imshow(img[..., 0])
